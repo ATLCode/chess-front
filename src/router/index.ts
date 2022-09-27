286e391b2bf6bcf0f4cc27234f0d4e03f5779d32
ATLCode <aleksanteri.leinonen@gmail.com>
2022-09-27 07:28:25 +0300

    Settings Page

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Login from "../views/Login.vue";
 import GameHistory from "../views/GameHistory.vue";
 import Statistics from "../views/Statistics.vue";
+import Settings from "../views/Settings.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: "/Statistics",
       name: "statistics",
       component: Statistics,
+    },
+    {
+      path: "/Settings",
+      name: "settings",
+      component: Settings,
     },
   ],
 });
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,12 +1,73 @@
 <template>
-  <div class="settings-container">Settings</div>
+  <div class="settings-container">
+    <v-card class="settings-card">
+      <v-tabs
+        v-model="tab"
+        background-color="primarylight"
+        slider-color="primary"
+        class="tabs"
+      >
+        <v-tab value="1">Chess</v-tab>
+        <v-tab value="2">Website</v-tab>
+        <v-tab value="3">Change Password</v-tab>
+      </v-tabs>
+
+      <v-window v-model="tab" class="window">
+        <v-window-item value="1" class="chess">
+          <v-form class="chess-form">
+            <v-text-field
+              variant="outlined"
+              label="Lichess Username"
+            ></v-text-field>
+          </v-form>
+          <v-btn color="primary">Save</v-btn>
+        </v-window-item>
+
+        <v-window-item value="2"> Two </v-window-item>
+
+        <v-window-item value="3"> Three </v-window-item>
+      </v-window>
+    </v-card>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const tab = ref(null);
+</script>
 
 <style scoped lang="scss">
 .settings-container {
   height: calc(100vh - 56px);
+  width: 100%;
   background-color: rgb(var(--v-theme-primarybg)) !important;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.settings-card {
+  height: 90%;
+  width: 90%;
+}
+.window {
+  height: 100%;
+  width: 100%;
+}
+.chess {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+}
+.chess-form {
+  height: 80%;
+  width: 80%;
+  display: flex;
+
+  justify-content: center;
+  align-items: center;
 }
 </style>
